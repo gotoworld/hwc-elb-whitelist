a1ca40fa65fd7170a572e6a2708370acd978631b
uname.chen <uname.chen@gmail.com>
2019-02-21 14:21:49 +0800

    update logback configuration

--- src/main/java/wt/devops/hwcejbwhitelist/WebConfig.java
+++ src/main/java/wt/devops/hwcejbwhitelist/WebConfig.java
@@ -22,6 +22,6 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(auditInterceptor).addPathPatterns("/huaweicloud/ejb/whitelist**");
+		registry.addInterceptor(auditInterceptor).addPathPatterns("/huaweicloud/ejb/whitelist/**");
 	}
 }
